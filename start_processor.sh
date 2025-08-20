fcf0a1e778611cf1157c925d170026c23c8efed8
tonythebroker <github.thank005@passmail.net>
2025-08-20 20:06:49 +0200

    replace docker compose in processor script

--- start_processor.sh
+++ start_processor.sh
@@ -4,7 +4,7 @@ set -e
 echo "⚡ Starting Spark Streaming Processor..."
 
 # Submit Spark job to the cluster
-docker-compose exec spark-master spark-submit \
+docker compose exec spark-master spark-submit \
     --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,org.postgresql:postgresql:42.7.0 \
     --master spark://spark-master:7077 \
     --executor-memory 1g \
